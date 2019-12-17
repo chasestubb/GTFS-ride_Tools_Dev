b9130115a0ab69edf0cd121d71b15a30584e4c90
pingnomw <pingnomw@oregonstate.edu>
2019-12-17 13:36:17 -0800

    Removed dependency on sb-admin-2.min.css

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 //import './index.css';
 import "./css/sb-admin-2.css"
-import "./css/sb-admin-2.min.css"
+//import "./css/sb-admin-2.min.css"
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
