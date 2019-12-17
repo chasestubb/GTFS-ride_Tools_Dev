448808f8149949daa9b78e77287bc565582173cc
pingnomw <pingnomw@oregonstate.edu>
2019-12-17 11:23:06 -0800

    Converted home page to React

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+//import './index.css';
+import "./css/sb-admin-2.css"
+import "./css/sb-admin-2.min.css"
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
