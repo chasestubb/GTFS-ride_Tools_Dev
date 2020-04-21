315289403436281badc42396d55c5a7cfd4381a9
AshleyVieira <43731021+AshleyVieira@users.noreply.github.com>
2020-04-21 15:10:06 -0700

    Simple unit test for feed creation

---
+++ backend/test/FeedCreationTests.js
@@ -0,0 +1,9 @@
+var agencyCreate = require('../feed_creation');
+var assert = require('assert');
+
+describe('Agency test', function(){
+    it('Agency test should return the proper number of agencies', function(){
+            var agencies = agencyCreate(1).length;
+            assert.equal(agencies, 1);
+    });
+});
